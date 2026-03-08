1370dd2b49736d291a6f0cb21a7665e83743f37a
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2026-03-08 08:36:57 -0300

    rename module

--- examples/cstring/cstring_test.go
+++ examples/cstring/cstring_test.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"testing"
 
-	wasm3 "github.com/matiasinsaurralde/go-wasm3"
+	wasm3 "github.com/tarcisiozf/go-wasm3"
 )
 
 var (
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/matiasinsaurralde/go-wasm3
+module github.com/tarcisiozf/go-wasm3
 
 go 1.25.0
